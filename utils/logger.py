2822bb8438349f6da27a7996c6d28cf44dccf608
louistrankhanhhung-arch <louis.trankhanhhung@gmail.com>
2025-12-22 18:53:36 +0700

    Add logger setup function with configurable level

---
+++ utils/logger.py
@@ -0,0 +1,11 @@
+import logging
+import os
+
+
+def setup_logger() -> logging.Logger:
+    level = os.getenv("LOG_LEVEL", "INFO").upper()
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    )
+    return logging.getLogger("amp_smc")
